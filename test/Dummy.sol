4e6078bef8abea7d1108cdad89fc6ec9d104b20e
Ilan <36084092+ilanolkies@users.noreply.github.com>
2019-11-18 01:45:28 -0300

    Create Dummy.sol

---
+++ test/Dummy.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+import "@ensdomains/ens/contracts/ENSRegistry.sol";
+import "@ensdomains/resolvers/contracts/PublicResolver.sol";
+
+contract dummy {
+    constructor() public {}
+}
